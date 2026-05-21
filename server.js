af298ab5f57dbad3910e55a41095503e40f4e418
Priyanka <guptapriya7171@gmail.com>
2026-05-21 20:15:16 +0530

    Adding Production Build

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const toDoRoutes = require('./routes/ToDoRoutes');
+const path = require('path');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api',authRoutes);
 app.use('/api/todo',toDoRoutes);
